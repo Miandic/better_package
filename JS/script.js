d48be9d13ed6914ac872484cad71aeaca3e52846
Miandic <misha.andreev.novo@gmail.com>
2021-11-05 15:02:43 +0300

    Finally js getting all data from fields

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,28 @@
+function checkJson (jsonPath){
+    return(fetch(jsonPath)
+        .then(response => response.json())
+        .then(data => {
+            return (data)
+        }))
+}
+let jsonData = checkJson('data.json')
 
-fetch('data.json')
-.then(response => response.json())
-.then(data => {
-    var d = data
-    console.log(d)
-    document.getElementById("name").innerHTML = d[0].Name
-    document.getElementById("cost").innerHTML = d[0].Cost
-    document.getElementById("date").innerHTML = d[0].Date
-    document.getElementById("name").className = 'vis'
-    document.getElementById("cost").className = 'vis'
-    document.getElementById("date").className = 'vis'
-})
+const interval = setInterval(() => {
+    if(jsonData != checkJson('data.json')){
+        jsonData = checkJson('data.json')
+        fetch('data.json')
+            .then(response => response.json())
+            .then(data => {
+                let d = data
+                console.log('Updated')
+                document.getElementById("name").innerHTML = d[0].Name
+                document.getElementById("cost").innerHTML = d[0].Cost
+                document.getElementById("date").innerHTML = d[0].Date
+                document.getElementById("name").className = 'vis'
+                document.getElementById("cost").className = 'vis'
+                document.getElementById("date").className = 'vis'
+            })
+    }
+}, 1000)
+
+
--- JS2/script.js
+++ JS2/script.js
@@ -1,27 +1,29 @@
-function checkJson (jsonPath){
-    return(fetch(jsonPath)
-        .then(response => response.json())
-        .then(data => {
-            return (data)
-        }))
-}
-let jsonData = checkJson('data.json')
+let lenghtInput = document.getElementById('lenght').value
+let widthInput = document.getElementById('width').value
+let highInput = document.getElementById('high').value
+let weightInput = document.getElementById('weight').value
+let city1Input = document.getElementById('city1').value
+let city2Input = document.getElementById('city2').value
+
 const interval = setInterval(() => {
-    if(jsonData != checkJson('data.json')){
-        jsonData = checkJson('data.json')
-        fetch('data.json')
-            .then(response => response.json())
-            .then(data => {
-                let d = data
-                document.getElementById("name").innerHTML = d[0].Name
-                document.getElementById("cost").innerHTML = d[0].Cost
-                document.getElementById("date").innerHTML = d[0].Date
-                document.getElementById("name").className = 'vis'
-                document.getElementById("cost").className = 'vis'
-                document.getElementById("date").className = 'vis'
-            })
-    }
-}, 1000)
+    lenghtInput = document.getElementById('lenght').value
+    widthInput = document.getElementById('width').value
+    highInput = document.getElementById('high').value
+    weightInput = document.getElementById('weight').value
+    city1Input = document.getElementById('city1').value
+    city2Input = document.getElementById('city2').value
+}, 500)
+
+function logInputs(){
+    console.log(lenghtInput)
+    console.log(widthInput)
+    console.log(highInput)
+    console.log(weightInput)
+    console.log(city1Input)
+    console.log(city2Input)
+    console.log(' ')
+}
+
 
 
 

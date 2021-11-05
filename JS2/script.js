e71219dc4fee5048d7cc6534a66d09ca4e8793bc
SrbskiDobrovilci <83087628+SrbskiDobrovilci@users.noreply.github.com>
2021-11-05 07:08:04 +0300

    Misha
    Nikita posral

---
+++ JS2/script.js
@@ -0,0 +1,27 @@
+function checkJson (jsonPath){
+    return(fetch(jsonPath)
+        .then(response => response.json())
+        .then(data => {
+            return (data)
+        }))
+}
+let jsonData = checkJson('data.json')
+const interval = setInterval(() => {
+    if(jsonData != checkJson('data.json')){
+        jsonData = checkJson('data.json')
+        fetch('data.json')
+            .then(response => response.json())
+            .then(data => {
+                let d = data
+                document.getElementById("name").innerHTML = d[0].Name
+                document.getElementById("cost").innerHTML = d[0].Cost
+                document.getElementById("date").innerHTML = d[0].Date
+                document.getElementById("name").className = 'vis'
+                document.getElementById("cost").className = 'vis'
+                document.getElementById("date").className = 'vis'
+            })
+    }
+}, 1000)
+
+
+

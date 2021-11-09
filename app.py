5f1cad3b5625830657b723a6a371744c8d95f8a8
Miandic <misha.andreev.novo@gmail.com>
2021-11-09 23:49:12 +0300

    Getting data from site to flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        city_from = request.form['city1']
+        city_to = request.form['city2']
+        length = request.form['length']
+        width = request.form['width']
+        high = request.form['high']
+        weight = request.form['weight']
+        cost = request.form['cost']
+        print("Debug: city_from -", city_from, "city_to -", city_to, "length -", length, "width -", width, "high -", high, "weight -", weight, "cost -", cost)
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

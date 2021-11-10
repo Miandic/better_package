90416dfb22f94a9cce39f5e55a8391a3ab41dccf
Miandic <misha.andreev.novo@gmail.com>
2021-11-10 10:43:39 +0300

    ready to merge with main

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for, request
-
+from flask import Flask, render_template, url_for, reque
 app = Flask(__name__)
 
 
@@ -13,8 +12,13 @@ def index():
         high = request.form['high']
         weight = request.form['weight']
         cost = request.form['cost']
-        print("Debug: city_from -", city_from, "city_to -", city_to, "length -", length, "width -", width, "high -", high, "weight -", weight, "cost -", cost)
-        return render_template('index.html')
+        print("Debug: city_from -", city_from, "city_to -", city_to, "length -", length, "width -", width, "high -",
+              high, "weight -", weight, "cost -", cost)
+
+        try:
+            return render_template('index.html', )
+        except:
+            return "Error: please input all data"
     else:
         return render_template('index.html')
 

2e04acebec69e290d7e4dbf79529121f00e0026b
YaroslavFHC <ch_yarosslav_05@mail.ru>
2021-12-01 15:36:28 +0300

    Society ([eq from YaroSlave)

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 import {$,jQuery} from 'jquery-3.5.1.min.js';
-
+//хуй
 let list = document.querySelector('.list');
-
+//хуй
 var flag = jQuery.parseJSON("data.json")
 while (flag["a"] == 0) {
     flag = jQuery.parseJSON("data.json")

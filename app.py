38e90e040a1309d83d6c9ec754801700cd6dae52
Miandic <misha.andreev.novo@gmail.com>
2021-12-17 00:43:02 +0300

    repaired send form, deleted garbage, added .gitignote. Parser working again

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 import Parser.main
 import json
-#print("Setting 'recieved_data' to placeholder stats")
+print("Setting 'recieved_data' to placeholder stats")
 recived_data = [0, 1, 2, 3, 4, 5, 6]
-#print("recived_data now is ", recived_data)
+print("recived_data now is ", recived_data)
 
 q = {
         "a": 0
 }
-with open("flag.json", "w") as write_file:
-    json.dump(q, write_file)
+#with open("flag.json", "w") as write_file:
+#    json.dump(q, write_file)
 
 
 
@@ -18,7 +18,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     global recived_data
-    #print("Entered 'Index' function with 'recived_data' on ", *send_data())
+    print("Entered 'Index' function with 'recived_data' on ", *send_data())
     if request.method == 'POST':
         city_from = request.form['city1']
         city_to = request.form['city2']
@@ -28,10 +28,10 @@ def index():
         weight = request.form['weight']
         cost = request.form['cost']
         recived_data = [city_from, city_to, length, width, high, weight, cost]
-        #print("Updated 'recived_data' to: ", *send_data(), " and importing 'Parser.main' file")
-        #print("Just imported 'Parser.main file with 'recived_data' on: ", *send_data())
+        print("Updated 'recived_data' to: ", *send_data(), " and importing 'Parser.main' file")
+        print("Just imported 'Parser.main file with 'recived_data' on: ", *send_data())
         parsed_data = Parser.main.parse(send_data())
-        #print("just get this data: ", *parsed_data)
+        print("just get this data: ", *parsed_data)
         try:
             return render_template('index.html', data=parsed_data)
         except:
@@ -43,10 +43,10 @@ def index():
 
 def send_data():
     global recived_data
-    #print("Entered 'send_data' function with 'recived_data' ", recived_data)
+    print("Entered 'send_data' function with 'recived_data' ", recived_data)
     return recived_data
 
-#print("Line 40 processing, right now in 'recieved_data': ", send_data())
+print("Line 40 processing, right now in 'recieved_data': ", send_data())
 
 
 if __name__ == '__main__':
